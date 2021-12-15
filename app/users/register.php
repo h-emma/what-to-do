f9d6199c77e0051b46c8ef1af5300cfd9a21aa31
h-emma <emmah92@hotmail.com>
2021-12-15 13:28:33 +0100

    Start on function that register new users

--- app/users/register.php
+++ app/users/register.php
@@ -4,6 +4,11 @@ declare(strict_types=1);
 
 require __DIR__ . '/../autoload.php';
 
+if (isset($_POST['username'])) {
+    $username = trim($_POST['username']);
+
+    $database = new PDO('sqlite:database.db');
+}
 // In this file we register a new user.
 
 redirect('/');
