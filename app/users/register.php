2397e5cf8a2c13055360aac1b60c3f5f8985e764
h-emma <emmah92@hotmail.com>
2021-12-15 20:20:42 +0100

    Add data that save registration info in the database

--- app/users/register.php
+++ app/users/register.php
@@ -4,11 +4,19 @@ declare(strict_types=1);
 
 require __DIR__ . '/../autoload.php';
 
-if (isset($_POST['username'])) {
-    $username = trim($_POST['username']);
+if (isset($_POST['username'], $_POST['email'], $_POST['password'])) {
+    $username = trim(filter_var($_POST['username'], FILTER_SANITIZE_STRING));
+    $email = trim($_POST['email']);
+    $passwordHach = password_hash($_POST['password'], PASSWORD_DEFAULT);
 
-    $database = new PDO('sqlite:database.db');
+    $statement = $database->prepare('INSERT INTO users (username, email, password) VALUES (:username, :email, :password)');
+    $statement->bindParam(':username', $username);
+    $statement->bindParam(':email', $email);
+    $statement->bindParam(':password', $passwordHach);
+
+    $statement->execute();
+    $user = $statement->fetch(PDO::FETCH_ASSOC);
 }
 // In this file we register a new user.
 
-redirect('/');
+redirect('/login.php');
