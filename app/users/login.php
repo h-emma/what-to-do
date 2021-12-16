4ec139d0f2e5ab9295d8c27f25075da61e5bff95
h-emma <emmah92@hotmail.com>
2021-12-16 15:54:09 +0100

    Add session

--- app/users/login.php
+++ app/users/login.php
@@ -21,19 +21,19 @@ if (isset($_POST['email'], $_POST['password'])) {
     if (!$user) {
         redirect('/login.php');
     }
+    $passmatch = password_verify($_POST['password'], $user['password']);
 
-    // If we found the user in the database, compare the given password from the
-    // request with the one in the database using the password_verify function.
-    if (password_verify($_POST['password'], $user['password'])) {
-        // If the password was valid we know that the user exists and provided
-        // the correct password. We can now save the user in our session.
-        // Remember to not save the password in the session!
-        unset($user['password']);
+    if ($passmatch) {
 
-        $_SESSION['user'] = $user;
+        $_SESSION['user'] = [
+            'id' => $user['id'],
+            'username' => $user['username'],
+            'email' => $user['email'],
+            'avatar_image' => $user['avatar_image']
+        ];
+    } else {
+        echo 'error!';
     }
 }
 
-// We should put this redirect in the end of this file since we always want to
-// redirect the user back from this file. We don't know
 redirect('/');
