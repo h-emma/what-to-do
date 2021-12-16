ef962cc0b734e5353cc23f4a9c738b063fc7d001
h-emma <emmah92@hotmail.com>
2021-12-16 12:55:43 +0100

    Add size max size on the $avatar

--- app/users/register.php
+++ app/users/register.php
@@ -23,19 +23,23 @@ if (isset($_FILES['avatar'])) {
     $avatar = $_FILES['avatar'];
 
     $avatar['name'] = date('Y-m-d') . $avatar['name'];
-    $direction = __DIR__ . '/avatar-uploads/' . $avatar['name'];
+    $direction = __DIR__ . '/avatar-uploads/' . '-' . $avatar['name'];
+
 
     if ($avatar['type'] !== 'image/png') {
         echo 'The image file needs to be an .png, please choose an another format';
+    }
+    if ($avatar['size'] >= 200000) {
+        echo 'The image are to big, max size is';
     } else {
         move_uploaded_file($avatar['tmp_name'], $direction);
-
-        $statement = $database->prepare('INSERT INTO users (avatar_image) VALUES (:avatar_image)');
-        $statement->bindParam(':avatar_image', $avatar);
-
-        $statement->execute();
-        $user = $statement->fetch(PDO::FETCH_ASSOC);
     };
+
+    $statement = $database->prepare('INSERT INTO users (avatar_image) VALUES (:avatar_image)');
+    $statement->bindParam(':avatar_image', $avatar);
+
+    $statement->execute();
+    $user = $statement->fetch(PDO::FETCH_ASSOC);
 };
 // In this file we register a new user.
 
