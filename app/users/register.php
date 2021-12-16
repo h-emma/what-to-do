0be767b9f4e2dbcb440830b8adae9ebaffdaee72
h-emma <emmah92@hotmail.com>
2021-12-16 15:56:22 +0100

    Add space between date and name on image, change if to elseif, try to add avatar_image to the users index page

--- app/users/register.php
+++ app/users/register.php
@@ -9,38 +9,31 @@ if (isset($_POST['username'], $_POST['email'], $_POST['password'])) {
     $email = trim($_POST['email']);
     $passwordHach = password_hash($_POST['password'], PASSWORD_DEFAULT);
 
-    //Här är något som inte funkar!
-    $statement = $database->prepare('INSERT INTO users (username, email, password) VALUES (:username, :email, :password)');
+    if (isset($_FILES['avatar'])) {
+        $avatar = $_FILES['avatar'];
+
+        $avatar['name'] = date('Y-m-d') . '-' . $avatar['name'];
+        $direction = __DIR__ . '/avatar-uploads/' . $avatar['name'];
+
+
+
+        if ($avatar['type'] !== 'image/png') {
+            echo 'The image file needs to be an .png, please choose an another format';
+        } elseif ($avatar['size'] >= 2097152) {
+            echo 'The image are to big, max size is 2MB';
+        } else {
+            move_uploaded_file($avatar['tmp_name'], $direction);
+        };
+    };
+
+    $statement = $database->prepare('INSERT INTO users (username, email, password, avatar_image) VALUES (:username, :email, :password, :avatar_image)');
     $statement->bindParam(':username', $username);
     $statement->bindParam(':email', $email);
     $statement->bindParam(':password', $passwordHach);
+    $avatarpath = 'avatar-uploads/' . $avatar['name'];
+    $statement->bindParam(':avatar_image', $avatarpath);
 
     $statement->execute();
-    $user = $statement->fetch(PDO::FETCH_ASSOC);
-}
-
-if (isset($_FILES['avatar'])) {
-    $avatar = $_FILES['avatar'];
-
-    $avatar['name'] = date('Y-m-d') . $avatar['name'];
-    $direction = __DIR__ . '/avatar-uploads/' . '-' . $avatar['name'];
-
-
-    if ($avatar['type'] !== 'image/png') {
-        echo 'The image file needs to be an .png, please choose an another format';
-    }
-    if ($avatar['size'] >= 200000) {
-        echo 'The image are to big, max size is';
-    } else {
-        move_uploaded_file($avatar['tmp_name'], $direction);
-    };
-
-    $statement = $database->prepare('INSERT INTO users (avatar_image) VALUES (:avatar_image)');
-    $statement->bindParam(':avatar_image', $avatar);
-
-    $statement->execute();
-    $user = $statement->fetch(PDO::FETCH_ASSOC);
 };
-// In this file we register a new user.
 
 redirect('/login.php');
