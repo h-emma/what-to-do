dfffb27fefd9a4dafa04b12b5b5ed3243bacea95
h-emma <emmah92@hotmail.com>
2021-12-17 16:48:14 +0100

    Remove ' , ' from the update user line

--- app/users/update-settings-for-user.php
+++ app/users/update-settings-for-user.php
@@ -4,15 +4,14 @@ declare(strict_types=1);
 
 require __DIR__ . '/../autoload.php';
 
-if (isset($_POST['email'], $_POST['password'])) {
+if (isset($_POST['email'])) {
     $email = trim($_POST['email']);
     $id = $_SESSION['user']['id'];
 
+    $statement = $database->prepare('UPDATE users SET email = :email WHERE id = :id');
 
-    $statement = $database->prepare('UPDATE users SET email = :email, WHERE id = :id');
-
-    $statement->bindParam(':email', $email);
     $statement->bindParam(':id', $id);
+    $statement->bindParam(':email', $email);
 
     $statement->execute();
 };
@@ -21,12 +20,12 @@ if (isset($_POST['password'])) {
     $passwordHach = password_hash($_POST['password'], PASSWORD_DEFAULT);
     $id = $_SESSION['user']['id'];
 
-    $statement = $database->prepare('UPDATE users SET password = :password, WHERE id = :id');
+    $statement = $database->prepare('UPDATE users SET password = :password WHERE id = :id');
 
-    $statement->bindParam(':email', $email);
+    $statement->bindParam(':password', $passwordHach);
     $statement->bindParam(':id', $id);
 
     $statement->execute();
 }
 
-redirect('/');
+redirect('/settings-for-user.php');
