7dc9adbeef6fcaad5f5a0d17a8d1e42c3fda14f2
h-emma <emmah92@hotmail.com>
2021-12-20 14:08:01 +0100

    Add a function to get the title of the list from the db

--- app/functions.php
+++ app/functions.php
@@ -7,3 +7,13 @@ function redirect(string $path)
     header("Location: ${path}");
     exit;
 }
+
+function getLists($id, $database)
+{
+
+    $statement = $database->query('SELECT * FROM lists WHERE user_id = :user_id;');
+    $statement->bindParam(':user_id', $id);
+    $statement->execute();
+    $lists = $statement->fetchAll(PDO::FETCH_ASSOC);
+    return $lists;
+}
