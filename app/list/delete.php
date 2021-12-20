280e2171f654db202b9a92ff715f13b497542214
h-emma <emmah92@hotmail.com>
2021-12-20 14:08:55 +0100

    Start to create so that the user can delete the list

--- app/list/delete.php
+++ app/list/delete.php
@@ -4,6 +4,12 @@ declare(strict_types=1);
 
 require __DIR__ . '/../autoload.php';
 
-// In this file we delete new posts in the database.
 
-redirect('/');
+$statement = $database->prepare('DELETE FROM lists WHERE id = :id;');
+
+$statement->bindParam(':id', $id);
+
+$statement->execute();
+
+
+redirect('/lists.php');
