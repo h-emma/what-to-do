410e9eee9bac0a666623a8866076ee24889e4add
h-emma <emmah92@hotmail.com>
2021-12-20 14:12:19 +0100

    Add the list to the page, add input for update and delete

--- lists.php
+++ lists.php
@@ -3,23 +3,38 @@
 
 <article>
     <h1>Add your to-do list</h1>
-    <!-- <div class="list-container">
-        <h2>Lists</h2>
-        <?php foreach ($lists as $list) : ?>
-            <p><?php echo $comment['name']; ?></p>
-        <?php endforeach; ?>
-    </div> -->
 
     </div>
     <div class="list-form">
         <form action="app/list/create.php" method="post">
             <div class="add-list">
-                <label for="text">Lists title</label>
-                <input class="form-control" type="text" name="lists-title" id="lists-title" placeholder="title" required>
+                <label for="title">Lists title</label>
+                <input class="form-control" type="text" name="title" id="title" placeholder="title" required>
             </div>
             <button type="submit" class="add-list">Add list</button>
         </form>
     </div>
+    <div class="list-form">
+        <form action="app/list/update.php" method="post">
+            <div class="add-list">
+                <label for="title">Update title on the list</label>
+                <input class="form-control" type="text" name="title" id="title" placeholder="title" required>
+            </div>
+            <button type="submit" class="uppdate-title-list">Update title</button>
+        </form>
+    </div>
+    <?php if (isset($_SESSION['user'])) : ?>
+        <?php
+        foreach (getLists($_SESSION['user']['id'], $database) as $list) : ?>
+            <p><?php echo $list['title']; ?></p>
+        <?php endforeach; ?>
+    <?php endif; ?>
 </article>
-
+<div class="list-form">
+    <form action="app/list/delete.php" method="post">
+        <div class="add-list">
+            <input type="hidden" name="delete-list" value="title">
+            <button type="submit">Delete list</button>
+    </form>
+</div>
 <?php require __DIR__ . '/views/footer.php'; ?>
