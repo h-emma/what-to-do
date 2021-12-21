78119853ebaabeb613568df5357083623e3142c7
h-emma <emmah92@hotmail.com>
2021-12-21 16:30:53 +0100

    Remove completed from the list

--- app/task/create.php
+++ app/task/create.php
@@ -8,16 +8,14 @@ if (isset($_POST['title'])) {
     $title = ($_POST['title']);
     $description = ($_POST['description']);
     $created = ($_POST['created']);
-    $completed = ($_POST['completed']);
     $deadline = ($_POST['deadline']);
     $id = $_SESSION['user']['id'];
 
-    $statement = $database->prepare('INSERT INTO tasks (title, description, created, completed, deadline, user_id) VALUES (:title, :description, :created, :completed, :deadline, :user_id)');
+    $statement = $database->prepare('INSERT INTO tasks (title, description, created, deadline, user_id) VALUES (:title, :description, :created, :deadline, :user_id)');
 
     $statement->bindParam(':title', $title);
     $statement->bindParam(':description', $description);
     $statement->bindParam(':created', $created);
-    $statement->bindParam('completed', $completed);
     $statement->bindParam('deadline', $deadline);
     $statement->bindParam(':user_id', $id);
 
