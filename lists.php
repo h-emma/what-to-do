52a7e7e7f290b7014af0b82dda8f20477ccdccc1
h-emma <emmah92@hotmail.com>
2021-12-22 12:31:04 +0100

    Add comments to know what that forms are for

--- lists.php
+++ lists.php
@@ -5,6 +5,7 @@
     <h1>To-do lists</h1>
 
     </div>
+    <!-- add title to list -->
     <div class="list-form">
         <form action="app/list/create.php" method="post">
             <div class="add-list">
@@ -14,6 +15,7 @@
             <button type="submit" class="add-list">Add list</button>
         </form>
     </div>
+    <!-- Update list with now title -->
     <div class="list-form">
         <form action="app/list/update.php" method="post">
             <div class="add-list">
@@ -23,6 +25,7 @@
             <button type="submit" class="uppdate-title-list">Update title</button>
         </form>
     </div>
+    <!-- loop out the lists -->
     <?php if (isset($_SESSION['user'])) : ?>
         <?php
         foreach (getLists($_SESSION['user']['id'], $database) as $list) : ?>
@@ -30,40 +33,51 @@
         <?php endforeach; ?>
     <?php endif; ?>
 </article>
+<!-- button to delete list -->
 <div class="list-form">
     <form action="app/list/delete.php" method="post">
         <div class="add-list">
-            <input type="hidden" name="delete-list" value="title">
+            <input type="hidden" name="delete-list" id="delete-list" value="<?= $list['id'] ?>">
             <button type="submit">Delete list</button>
     </form>
 </div>
+<!-- Checkbox to click in if task is completed -->
 <div class="task-form">
     <form action="app/task/completed.php" method="post">
         <div class="add-task-completed">
-            <label for="checkbox-task-completed"></label>
-            <input type="checkbox" name="checkbox-task-completed" id="checkbox-task-completed">
+            <label for="completed"></label>
+            <input type="checkbox" name="completed[$id]" id="completed" value="1">
     </form>
 </div>
+<!-- Form to create task -->
 <div class="task-form">
     <form action="app/task/create.php" method="post">
         <div class="add-task">
-            <label for="title">Task title</label>
+            <label for="task-title">Task title</label>
             <input class="form-control" type="text" name="task-title" id="task-title" placeholder="title" required>
         </div>
         <div class="add-task">
-            <label for="description">Task description</label>
+            <label for="task-description">Task description</label>
             <input class="form-control" type="text" name="task-description" id="task-description" placeholder="description" required>
         </div>
         <div class="add-task">
-            <label for="created">Created at</label>
+            <label for="task-created">Created at</label>
             <input class="form-control" type="date" name="task-created" id="task-created" required>
         </div>
         <div class="add-task">
-            <label for="deadline">Deadline</label>
+            <label for="task-deadline">Deadline</label>
             <input class="form-control" type="date" name="task-deadline" id="task-deadline" required>
         </div>
         <button type="submit" class="add-task">Add task to list</button>
     </form>
 </div>
+<!-- Loop out tasks -->
+<?php
+foreach (getTasksInList($_SESSION['user']['id'], $database) as $task) : ?>
+    <p><?php echo $task['title']; ?></p>
+    <p><?php echo $task['description']; ?></p>
+    <p><?php echo $task['created']; ?></p>
+    <p><?php echo $task['deadline']; ?></p>
+<?php endforeach; ?>
 
 <?php require __DIR__ . '/views/footer.php'; ?>
