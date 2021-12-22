8a57668ae2825b9e12d40d63ce7fe3a892cb823f
h-emma <emmah92@hotmail.com>
2021-12-22 14:41:48 +0100

    Add completed to the input for the task

--- app/task/create.php
+++ app/task/create.php
@@ -9,14 +9,16 @@ if (isset($_POST['task-title'])) {
     $description = ($_POST['task-description']);
     $created = ($_POST['task-created']);
     $deadline = ($_POST['task-deadline']);
+    $completed = ($_POST['completed']);
     $id = $_SESSION['user']['id'];
 
-    $statement = $database->prepare('INSERT INTO tasks (title, description, created, deadline, user_id) VALUES (:title, :description, :created, :deadline, :user_id)');
+    $statement = $database->prepare('INSERT INTO tasks (title, description, created, deadline, completed, user_id) VALUES (:title, :description, :created, :deadline, :completed, :user_id)');
 
     $statement->bindParam(':title', $title);
     $statement->bindParam(':description', $description);
     $statement->bindParam(':created', $created);
     $statement->bindParam('deadline', $deadline);
+    $statement->bindParam('completed', $completed);
     $statement->bindParam(':user_id', $id);
 
     $statement->execute();
