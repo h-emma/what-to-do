6d3c7c85779c846c9f9a67e0541106aed1603741
h-emma <emmah92@hotmail.com>
2021-12-28 15:03:40 +0100

    Add sanitize and what kind of data type that is output

--- app/task/create.php
+++ app/task/create.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 require __DIR__ . '/../autoload.php';
 
 if (isset($_POST['task-title'])) {
-    $title = ($_POST['task-title']);
+    $title = trim(filter_var($_POST['task-title'], FILTER_SANITIZE_STRING));
     $description = ($_POST['task-description']);
     $created = ($_POST['task-created']);
     $deadline = ($_POST['task-deadline']);
@@ -14,12 +14,12 @@ if (isset($_POST['task-title'])) {
 
     $statement = $database->prepare('INSERT INTO tasks (title, description, created, deadline, completed, user_id) VALUES (:title, :description, :created, :deadline, :completed, :user_id)');
 
-    $statement->bindParam(':title', $title);
-    $statement->bindParam(':description', $description);
-    $statement->bindParam(':created', $created);
-    $statement->bindParam('deadline', $deadline);
-    $statement->bindParam('completed', $completed);
-    $statement->bindParam(':user_id', $id);
+    $statement->bindParam(':title', $title, PDO::PARAM_STR);
+    $statement->bindParam(':description', $description, PDO::PARAM_STR);
+    $statement->bindParam(':created', $created, PDO::PARAM_STR);
+    $statement->bindParam('deadline', $deadline, PDO::PARAM_STR);
+    $statement->bindParam('completed', $completed, PDO::PARAM_STR);
+    $statement->bindParam(':user_id', $id, PDO::PARAM_INT);
 
     $statement->execute();
     $user = $statement->fetch(PDO::FETCH_ASSOC);
