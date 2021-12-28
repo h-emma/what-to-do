93ce10bad8bbf02506087bf46fc217db84b174e4
h-emma <emmah92@hotmail.com>
2021-12-28 22:32:13 +0100

    Add function for isset($_SESSION['user'])

--- app/functions.php
+++ app/functions.php
@@ -6,31 +6,38 @@ function redirect(string $path)
 {
     header("Location: ${path}");
     exit;
-}
+};
+// Functions for when user is logged in
+function isUserLoggedIn()
+{
+    $loggedIn = isset($_SESSION['user']);
+    return $loggedIn;
+};
 // Function that get the lists
 function getLists($id, $database)
 {
     $statement = $database->query('SELECT * FROM lists WHERE user_id = :user_id;');
-    $statement->bindParam(':user_id', $id);
+    $statement->bindParam(':user_id', $id, PDO::PARAM_INT);
     $statement->execute();
     $lists = $statement->fetchAll(PDO::FETCH_ASSOC);
     return $lists;
-}
+};
 // Funciton that get tasks in the list
 function getTasksInList($id, $database)
 {
     $statement = $database->query('SELECT * FROM tasks WHERE user_id = :user_id;');
-    $statement->bindParam(':user_id', $id);
+    $statement->bindParam(':user_id', $id, PDO::PARAM_INT);
     $statement->execute();
     $listTasks = $statement->fetchAll(PDO::FETCH_ASSOC);
     return $listTasks;
-}
+};
 // Function that get all tasks that have deadline today
 function getTasksDeadlineToday($id, $database)
 {
-    $statement = $database->query('SELECT * FROM tasks WHERE deadline = CURDATE();');
-    $statement->bindParam('CURDATE()', $id);
+
+    $statement = $database->query('SELECT * FROM tasks WHERE user_id = :user_id AND deadline IS CURDATE();');
+    $statement->bindParam(':user_id', $id, PDO::PARAM_INT);
     $statement->execute();
     $deadlineTodayTasks = $statement->fetchAll(PDO::FETCH_ASSOC);
     return $deadlineTodayTasks;
-}
+};
