602f398ebc55abf343d0a92474539ca514cbf47f
h-emma <emmah92@hotmail.com>
2021-12-28 22:37:04 +0100

    Change php echo to =

--- views/navigation.php
+++ views/navigation.php
@@ -1,5 +1,5 @@
 <nav class="nav-container">
-    <a class="nav-loga" href="#"><?php echo $config['title']; ?></a>
+    <a class="nav-loga" href="#"><?= $config['title']; ?></a>
     <div class="hamburger">
         <span class="bar"></span>
         <span class="bar"></span>
@@ -7,18 +7,18 @@
     </div>
     <ul class="navbar-nav">
         <li class="nav-item">
-            <a class="nav-link <?php echo $_SERVER['SCRIPT_NAME'] === '/index.php' ? 'active' : ''; ?>" href="/index.php">Home</a>
+            <a class="nav-link <?= $_SERVER['SCRIPT_NAME'] === '/index.php' ? 'active' : ''; ?>" href="/index.php">Home</a>
         </li>
 
         <!-- <li class="nav-item">
-            <a class="nav-link <?php echo $_SERVER['SCRIPT_NAME'] === '/about.php' ? 'active' : ''; ?>" href="/about.php">About</a>
+            <a class="nav-link <?= $_SERVER['SCRIPT_NAME'] === '/about.php' ? 'active' : ''; ?>" href="/about.php">About</a>
         </li> -->
 
         <li class="nav-item">
             <?php if (isset($_SESSION['user'])) : ?>
                 <a class="nav-link" href="/app/users/logout.php">Logout</a>
             <?php else : ?>
-                <a class="nav-link <?php echo $_SERVER['SCRIPT_NAME'] === '/login.php' ? 'active' : ''; ?>" href="login.php">Login</a>
+                <a class="nav-link <?= $_SERVER['SCRIPT_NAME'] === '/login.php' ? 'active' : ''; ?>" href="login.php">Login</a>
             <?php endif; ?>
         </li>
 
@@ -26,11 +26,12 @@
             <?php if (isset($_SESSION['user'])) : ?>
                 <a class="nav-link" href="settings-for-user.php">Settings</a>
             <?php else : ?>
-                <a class="nav-link <?php echo $_SERVER['SCRIPT_NAME'] === '/create-account.php' ? 'active' : ''; ?>" href="/create-account.php">Create an account</a>
+                <a class="nav-link <?= $_SERVER['SCRIPT_NAME'] === '/create-account.php' ? 'active' : ''; ?>" href="/create-account.php">Create an account</a>
             <?php endif; ?>
         </li>
 
         <li class="nav-item">
+            <!-- varför funkat det inte?! ($loggedIn) -->
             <?php if (isset($_SESSION['user'])) : ?>
                 <a class="nav-link" href="lists.php">To-do lists</a>
             <?php endif; ?>
