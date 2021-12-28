4591d835ed9e2223cf2e0f0bbf19e7ce9026c0d8
h-emma <emmah92@hotmail.com>
2021-12-28 15:03:12 +0100

    Add sanitize and what kind of data type that is output

--- app/list/update.php
+++ app/list/update.php
@@ -5,13 +5,13 @@ declare(strict_types=1);
 require __DIR__ . '/../autoload.php';
 
 if (isset($_POST['title'])) {
-    $title = ($_POST['title']);
+    $title = trim(filter_var($_POST['title'], FILTER_SANITIZE_STRING));
     $id = $_SESSION['user']['id'];
 
     $statement = $database->prepare('UPDATE lists SET title = :title WHERE id = :id');
 
-    $statement->bindParam(':id', $id);
-    $statement->bindParam(':title', $title);
+    $statement->bindParam(':id', $id, PDO::PARAM_INT);
+    $statement->bindParam(':title', $title, PDO::PARAM_STR);
 
     $statement->execute();
 };
