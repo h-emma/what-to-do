5e03d8e2ebe9cc4cd0a01a0a5361ce309afd9a69
h-emma <emmah92@hotmail.com>
2021-12-28 15:03:31 +0100

    Add what kind of data type that is output

--- app/task/completed.php
+++ app/task/completed.php
@@ -10,8 +10,8 @@ if (isset($_POST['completed'])) {
 
     $statement = $database->prepare('UPDATE tasks SET completed = :completed WHERE id = :id');
 
-    $statement->bindParam(':id', $id);
-    $statement->bindParam(':completed', $completed);
+    $statement->bindParam(':id', $id, PDO::PARAM_INT);
+    $statement->bindParam(':completed', $completed, PDO::PARAM_STR);
 
     $statement->execute();
 };
