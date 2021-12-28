df38969a311572add609ee033498ead35e146d22
h-emma <emmah92@hotmail.com>
2021-12-28 15:02:47 +0100

    Add sanitize and what kind of data type that is output

--- app/list/create.php
+++ app/list/create.php
@@ -5,12 +5,13 @@ declare(strict_types=1);
 require __DIR__ . '/../autoload.php';
 
 if (isset($_POST['title'])) {
-    $title = ($_POST['title']);
+    $title = trim(filter_var($_POST['title'], FILTER_SANITIZE_STRING));
     $id = $_SESSION['user']['id'];
+
     $statement = $database->prepare('INSERT INTO lists (title, user_id) VALUES (:title, :user_id)');
 
-    $statement->bindParam(':title', $title);
-    $statement->bindParam(':user_id', $id);
+    $statement->bindParam(':title', $title, PDO::PARAM_STR);
+    $statement->bindParam(':user_id', $id, PDO::PARAM_INT);
 
     $statement->execute();
     $user = $statement->fetch(PDO::FETCH_ASSOC);
