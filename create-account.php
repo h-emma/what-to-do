f043b4554e8c5f8aaa5cb97c3de2d48b807948f3
h-emma <emmah92@hotmail.com>
2021-12-28 15:07:52 +0100

    Update class on button, add more date type on avatar image

--- create-account.php
+++ create-account.php
@@ -8,25 +8,24 @@
         <div class="register-username">
             <label for="username">Username</label>
             <input class="form-control" type="text" name="username" id="username" placeholder="Username" required>
-            <small class="form-text"></small>
         </div>
 
         <div class="register-emial">
             <label for="email">Email</label>
             <input class="form-control" type="email" name="email" id="email" placeholder="email@mail.com" required>
-            <!-- <small class="form-text"></small> -->
+
         </div>
 
         <div class=register-password>
             <label for="password">Password</label>
             <input class="form-control" type="password" name="password" id="password" placeholder="Password" required>
-            <!-- <small class="form-text"></small> -->
+
         </div>
         <div class=register-avatar>
-            <label for="avatar">Upload an image in PNG format</label>
-            <input class="form-control" type="file" name="avatar" id="avatar" accept=".png" required>
+            <label for="avatar">Upload an image</label>
+            <input class="form-control" type="file" name="avatar" id="avatar" accept=".png, .jpg, .jpeg" required>
         </div>
-        <button type="submit" class="button-create-account">Create an account</button>
+        <button type="submit" class="button-main">Create an account</button>
     </form>
 
     <p>Do you have an account, <a href="login.php">login!</a></p>
