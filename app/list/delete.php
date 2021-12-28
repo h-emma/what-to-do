55f87c760502dbb2612e124d10b4786aba6d6f23
h-emma <emmah92@hotmail.com>
2021-12-28 15:03:08 +0100

    Add what kind of data type that is output

--- app/list/delete.php
+++ app/list/delete.php
@@ -8,7 +8,7 @@ $id = $_POST['delete-list'];
 
 $statement = $database->prepare('DELETE FROM lists WHERE id = :id;');
 
-$statement->bindParam(':id', $id);
+$statement->bindParam(':id', $id, PDO::PARAM_INT);
 
 $statement->execute();
 
