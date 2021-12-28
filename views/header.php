241354026a682bc4567dd37b9cb465d5a434e453
h-emma <emmah92@hotmail.com>
2021-12-28 22:36:47 +0100

    Add stylesheet

--- views/header.php
+++ views/header.php
@@ -5,10 +5,14 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <title><?php echo $config['title']; ?></title>
+    <title><?= $config['title']; ?></title>
 
-    <link rel="stylesheet" href="/assets/styles/app.css">
     <link rel="stylesheet" href="/assets/styles/media.css">
+    <link rel="stylesheet" href="/assets/styles/typography.css">
+    <link rel="stylesheet" href="/assets/styles/navigation.css">
+    <link rel="stylesheet" href="/assets/styles/global.css">
+    <link rel="stylesheet" href="/assets/styles/button.css">
+    <link rel="stylesheet" href="/assets/styles/input-form.css">
 </head>
 
 <body>
