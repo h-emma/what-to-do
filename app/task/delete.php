0b9e4a5539ebe529bf1cd1dd413774e6bf494185
h-emma <emmah92@hotmail.com>
2021-12-28 15:03:51 +0100

    Add what kind of data type that is output

--- app/task/delete.php
+++ app/task/delete.php
@@ -8,7 +8,7 @@ $id = $_POST['delete-task'];
 
 $statement = $database->prepare('DELETE FROM tasks WHERE id = :id;');
 
-$statement->bindParam(':id', $id);
+$statement->bindParam(':id', $id, PDO::PARAM_INT);
 
 $statement->execute();
 
