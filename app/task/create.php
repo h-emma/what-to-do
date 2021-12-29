4cddac1f24d17e80b1cfb82615074bdf857697b2
h-emma <emmah92@hotmail.com>
2021-12-29 16:16:44 +0100

    Update create.php

--- app/task/create.php
+++ app/task/create.php
@@ -9,16 +9,20 @@ if (isset($_POST['task-title'])) {
     $description = ($_POST['task-description']);
     $created = ($_POST['task-created']);
     $deadline = ($_POST['task-deadline']);
-    $completed = ($_POST['completed']);
+    if (isset($_POST['completed'])) {
+        $completed = ($_POST['completed']);
+    };
+    $inList = ($_POST['list']);
     $id = $_SESSION['user']['id'];
 
-    $statement = $database->prepare('INSERT INTO tasks (title, description, created, deadline, completed, user_id) VALUES (:title, :description, :created, :deadline, :completed, :user_id)');
+    $statement = $database->prepare('INSERT INTO tasks (title, description, created, deadline, completed, list_id, user_id) VALUES (:title, :description, :created, :deadline, :completed, :list_id, :user_id)');
 
     $statement->bindParam(':title', $title, PDO::PARAM_STR);
     $statement->bindParam(':description', $description, PDO::PARAM_STR);
     $statement->bindParam(':created', $created, PDO::PARAM_STR);
-    $statement->bindParam('deadline', $deadline, PDO::PARAM_STR);
-    $statement->bindParam('completed', $completed, PDO::PARAM_STR);
+    $statement->bindParam(':deadline', $deadline, PDO::PARAM_STR);
+    $statement->bindParam(':completed', $completed, PDO::PARAM_STR);
+    $statement->bindParam(':list_id', $inList, PDO::PARAM_INT);
     $statement->bindParam(':user_id', $id, PDO::PARAM_INT);
 
     $statement->execute();
