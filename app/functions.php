247c7260b30068a5d4d3d51b5f3aa111b51c0172
h-emma <emmah92@hotmail.com>
2021-12-29 16:16:14 +0100

    Add function that get all tasks with deadline today and function that get task in list

--- app/functions.php
+++ app/functions.php
@@ -25,7 +25,8 @@ function getLists($id, $database)
 // Funciton that get tasks in the list
 function getTasksInList($id, $database)
 {
-    $statement = $database->query('SELECT * FROM tasks WHERE user_id = :user_id;');
+    $statement = $database->query('SELECT * FROM tasks WHERE user_id = :user_id AND list_id = :list_id;');
+    $statement->bindParam(':list_id', $_GET['list-page'], PDO::PARAM_INT);
     $statement->bindParam(':user_id', $id, PDO::PARAM_INT);
     $statement->execute();
     $listTasks = $statement->fetchAll(PDO::FETCH_ASSOC);
@@ -34,10 +35,12 @@ function getTasksInList($id, $database)
 // Function that get all tasks that have deadline today
 function getTasksDeadlineToday($id, $database)
 {
-
-    $statement = $database->query('SELECT * FROM tasks WHERE user_id = :user_id AND deadline IS CURDATE();');
+    $statement = $database->query('SELECT * FROM tasks WHERE user_id = :user_id');
     $statement->bindParam(':user_id', $id, PDO::PARAM_INT);
     $statement->execute();
     $deadlineTodayTasks = $statement->fetchAll(PDO::FETCH_ASSOC);
+    $deadlineTodayTasks = array_filter($deadlineTodayTasks, function ($deadlineTodayTask) {
+        return $deadlineTodayTask['deadline'] === date('Y-m-d');
+    });
     return $deadlineTodayTasks;
 };
