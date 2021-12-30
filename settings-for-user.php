f360fbccb7e702fbe358a43c4e7e9bd90bebfba5
h-emma <emmah92@hotmail.com>
2021-12-30 21:36:53 +0100

    Remove update image

--- settings-for-user.php
+++ settings-for-user.php
@@ -19,14 +19,6 @@
         </div>
         <button type="submit" class="button-main">Update your password</button>
     </form>
-    <!--
-    <form action="app/users/edit-settings-for-user.php" method="post" enctype="multipart/form-data">
-        <div class=update-avatar>
-            <label for="avatar">Upload an image in PNG format</label>
-            <input class="form-control" type="file" name="avatar" id="avatar" accept=".png" required>
-        </div> -->
-    <!-- <button type="submit" class="button-update-settings">Update</button>
-    </form> -->
 </article>
 
 <?php require __DIR__ . '/views/footer.php'; ?>
