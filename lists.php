ffbb4d9b746ed38b98f7470ca5f1d7669948f754
h-emma <emmah92@hotmail.com>
2021-12-30 21:36:15 +0100

    Add delete button on all the lists

--- lists.php
+++ lists.php
@@ -2,7 +2,7 @@
 <?php require __DIR__ . '/views/header.php'; ?>
 
 <article>
-    <h1>To-do lists</h1>
+    <h1>Create your to-do lists</h1>
 
     </div>
     <!-- add title to list -->
@@ -16,38 +16,30 @@
         </form>
     </div>
     <!-- Update list with new title -->
-    <div class="list-form">
-        <form action="app/list/update.php" method="post">
-            <div class="add-list">
-                <label for="title">Update title on the list</label>
-                <input class="form-control" type="text" name="title" id="title" placeholder="title" required>
-            </div>
-            <button type="submit" class="button-main">Update title</button>
-        </form>
-    </div>
-    <!-- loop out the lists -->
-    <?php if (isset($_SESSION['user'])) : ?>
-        <?php
-        foreach (getLists($_SESSION['user']['id'], $database) as $list) : ?>
-            <form action="list.php" method="GET">
-                <div class="list">
-                    <input type="hidden" name="list-page" id="list-page" value="<?= $list['id'] ?>">
-                    <input type="hidden" name="list-name" id="list-name" value="<?= $list['title'] ?>">
-                    <button type="submit" class="button-main"><?= $list['title'] ?></button>
-                </div>
-            </form>
 
-        <?php endforeach; ?>
-    <?php endif; ?>
+    <!-- loop out the lists -->
+    <?php
+    foreach (getLists($_SESSION['user']['id'], $database) as $list) : ?>
+        <form action="list.php" method="GET">
+            <div class="list">
+                <input type="hidden" name="list-page" id="list-page" value="<?= $list['id'] ?>">
+                <input type="hidden" name="list-name" id="list-name" value="<?= $list['title'] ?>">
+                <button type="submit" class="button-main"><?= $list['title'] ?></button>
+            </div>
+        </form>
+
+    <?php endforeach; ?>
 </article>
 <!-- button to delete list -->
 <div class="list-form">
-    <form action="app/list/delete.php" method="post">
-        <div class="add-list">
-            <input type="hidden" name="delete-list" id="delete-list" value="<?= $list['id'] ?>">
-            <button type="submit" class="button-main">Delete list</button>
-        </div>
-    </form>
+    <?php foreach (getLists($_SESSION['user']['id'], $database) as $list) : ?>
+        <form action="app/list/delete.php" method="post">
+            <div class="add-list">
+                <input type="hidden" name="delete-list" id="delete-list" value="<?= $list['id'] ?>">
+                <button type="submit" class="button-main">Delete <?= $list['title'] ?></button>
+            </div>
+        </form>
+    <?php endforeach; ?>
 </div>
 <!-- Form to create task -->
 <div class="task-form">
@@ -61,57 +53,20 @@
             <input class="form-control" type="text" name="task-description" id="task-description" placeholder="description" required>
         </div>
         <div class="add-task">
-            <label for="task-created">Created at</label>
-            <input class="form-control" type="date" name="task-created" id="task-created" required>
-        </div>
-        <div class="add-task">
             <label for="task-deadline">Deadline</label>
             <input class="form-control" type="date" name="task-deadline" id="task-deadline" required>
         </div>
-        <!-- Checkbox to click in if task is completed -->
-        <div class="add-task">
-            <form action="app/task/completed.php" method="post">
-                <div class="add-task-completed">
-                    <label for="completed"></label>
-                    <input type="checkbox" name="completed" id="completed" value="1">
-            </form>
-        </div>
         <div>
-            <label for="select-list">Choose a list:</label>
+            <label for="select-list">Choose a list to add the task to:</label>
 
             <select name="list" id="select-list">
                 <?php foreach (getLists($_SESSION['user']['id'], $database) as $list) : ?>
-                    <?php $listId = $list['id'] ?>
-                    <option value="<?= $listId ?>"><?= $list['title']; ?></option>
+                    <option value="<?= $list['id'] ?>"><?= $list['title']; ?></option>
                 <?php endforeach; ?>
             </select>
         </div>
         <button type="submit" class="button-main">Add task to list</button>
     </form>
 </div>
-<!-- Loop out tasks -->
-<!-- <?php
-        foreach (getTasksInList($_SESSION['user']['id'], $database) as $task) : ?>
-    <p><?= $task['title']; ?></p>
-    <p><?= $task['description']; ?></p>
-    <p><?= $task['created']; ?></p>
-    <p><?= $task['deadline']; ?></p>
-    <p><?= $task['completed']; ?></p>
-    <div class="add-task">
-        <form action="app/task/completed.php" method="post">
-            <div class="add-task-completed">
-                <label for="completed"></label>
-                <input type="checkbox" name="completed" id="completed" value="1">
-        </form>
-    </div>
-<?php endforeach; ?>
-button to delete task
-<div class="list-form">
-    <form action="app/task/delete.php" method="post">
-        <div class="add-task">
-            <input type="hidden" name="delete-task" id="delete-task" value="<?= $task['id'] ?>">
-            <button type="submit" class="button-main">Delete task</button>
-    </form>
-</div> -->
 
 <?php require __DIR__ . '/views/footer.php'; ?>
