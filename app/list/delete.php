57148df420c6c49964a6b3ed123e32781f3848b8
h-emma <emmah92@hotmail.com>
2022-01-02 13:41:26 +0100

    Make two queries to delete both list and tasks

--- app/list/delete.php
+++ app/list/delete.php
@@ -3,14 +3,18 @@
 declare(strict_types=1);
 
 require __DIR__ . '/../autoload.php';
+if (isset($_POST['delete-list'])) {
+    $id = $_POST['delete-list'];
 
-$id = $_POST['delete-list'];
+    $statement = $database->prepare('DELETE FROM lists WHERE id = :id;');
 
-$statement = $database->prepare('DELETE lists, tasks FROM lists INNER JOIN tasks IN lists.id = tasks.list_id WHERE id = :id;');
+    $statement->bindParam(':id', $id, PDO::PARAM_INT);
 
-$statement->bindParam(':id', $id, PDO::PARAM_INT);
+    $statement->execute();
 
-$statement->execute();
+    $statement = $database->prepare('DELETE FROM tasks WHERE list_id = :list_id;');
 
-
+    $statement->bindParam(':list_id', $id, PDO::PARAM_INT);
+    $statement->execute();
+};
 redirect('/lists.php');
