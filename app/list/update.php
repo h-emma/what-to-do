e389b6fc922d0526111cbeac1c8517ac3a038243
h-emma <emmah92@hotmail.com>
2022-01-03 14:45:46 +0100

    Add redirect to the list that the title is change

--- app/list/update.php
+++ app/list/update.php
@@ -6,13 +6,15 @@ require __DIR__ . '/../autoload.php';
 
 if (isset($_POST['title'])) {
     $title = trim(filter_var($_POST['title'], FILTER_SANITIZE_STRING));
-    $id = $_SESSION['user']['id'];
+    $userId = $_SESSION['user']['id'];
+    $listId = ($_POST['list-id']);
 
-    $statement = $database->prepare('UPDATE lists SET title = :title WHERE id = :id');
+    $statement = $database->prepare('UPDATE lists SET title = :title WHERE id = :id AND user_id = :user_id');
 
-    $statement->bindParam(':id', $id, PDO::PARAM_INT);
+    $statement->bindParam(':user_id', $userId, PDO::PARAM_INT);
     $statement->bindParam(':title', $title, PDO::PARAM_STR);
+    $statement->bindParam(':id', $listId, PDO::PARAM_INT);
 
     $statement->execute();
+    redirect("/list.php?list-page=$listId&list-name=$title");
 };
-redirect('/lists.php');
