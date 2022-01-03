a940da5011594f19380d1fa87d7f8787f098a67a
h-emma <emmah92@hotmail.com>
2022-01-03 14:48:44 +0100

    Add query that get list id (does not work)

--- app/task/delete.php
+++ app/task/delete.php
@@ -6,11 +6,20 @@ require __DIR__ . '/../autoload.php';
 
 if (isset($_POST['delete-task'])) {
     $id = $_POST['delete-task'];
+    $inList = ($_POST['list']);
+
 
     $statement = $database->prepare('DELETE FROM tasks WHERE id = :id;');
 
     $statement->bindParam(':id', $id, PDO::PARAM_INT);
 
     $statement->execute();
+
+    $statement = $database->prepare('SELECT * FROM lists WHERE id = :list');
+    $statement->bindParam(':list', $inList, PDO::PARAM_INT);
+
+    $statement->execute();
+    $list = $statement->fetch(PDO::FETCH_ASSOC);
+    die(var_dump($list));
+    redirect("/list.php?list-page=$inList&list-name=$list[title]");
 };
-redirect('/list.php');
