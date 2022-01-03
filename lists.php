969051a328f7f2291c4133594e1c7b968dc5b089
h-emma <emmah92@hotmail.com>
2022-01-03 14:54:22 +0100

    Remove create task form from lists.php, add in to list.php

--- lists.php
+++ lists.php
@@ -15,8 +15,6 @@
             <button type="submit" class="button-main">Add list</button>
         </form>
     </div>
-    <!-- Update list with new title -->
-
     <!-- loop out the lists -->
     <?php
     foreach (getLists($_SESSION['user']['id'], $database) as $list) : ?>
@@ -41,32 +39,4 @@
         </form>
     <?php endforeach; ?>
 </div>
-<!-- Form to create task -->
-<div class="task-form">
-    <form action="app/task/create.php" method="post">
-        <div class="add-task">
-            <label for="task-title">Task title</label>
-            <input class="form-control" type="text" name="task-title" id="task-title" placeholder="title" required>
-        </div>
-        <div class="add-task">
-            <label for="task-description">Task description</label>
-            <input class="form-control" type="text" name="task-description" id="task-description" placeholder="description" required>
-        </div>
-        <div class="add-task">
-            <label for="task-deadline">Deadline</label>
-            <input class="form-control" type="date" name="task-deadline" id="task-deadline" required>
-        </div>
-        <div>
-            <label for="select-list">Choose a list to add the task to:</label>
-
-            <select name="list" id="select-list">
-                <?php foreach (getLists($_SESSION['user']['id'], $database) as $list) : ?>
-                    <option value="<?= $list['id'] ?>"><?= $list['title']; ?></option>
-                <?php endforeach; ?>
-            </select>
-        </div>
-        <button type="submit" class="button-main">Add task to list</button>
-    </form>
-</div>
-
 <?php require __DIR__ . '/views/footer.php'; ?>
