66d77542621ef12566d80df7cd4f397050912e58
h-emma <emmah92@hotmail.com>
2022-01-03 14:52:43 +0100

    Add text if user is not logged in, change place on image and welcome and delete button to task

--- index.php
+++ index.php
@@ -1,31 +1,44 @@
 <?php require __DIR__ . '/app/autoload.php'; ?>
 <?php require __DIR__ . '/views/header.php'; ?>
 
-<article>
+<main>
     <h1><?php echo $config['title']; ?></h1>
+    <!-- If the user is not logged in show this -->
+    <?php
+    if (!isset($_SESSION['user'])) : ?>
+        <p>Create an account and log in to start create your to-do lists!</p>
+    <?php endif; ?>
 
     <?php if (isset($_SESSION['user'])) : ?>
-        <p>Welcome, <?= htmlspecialchars($_SESSION['user']['username']); ?>!</p>
-        <img src="<?= $_SESSION['user']['avatar_image']; ?>" alt="Your profile image">
+        <div class="user-container">
+            <img src="<?= $_SESSION['user']['avatar_image']; ?>" alt="Your profile image">
+            <h2>Welcome, <?= htmlspecialchars($_SESSION['user']['username']); ?>!</h2>
+        </div>
 
         <!-- Loop out the task that have deadline today -->
-        <?php
-        foreach (getTasksDeadlineToday($_SESSION['user']['id'], $database) as $deadlineTodayTasks) : ?>
-            <p><?= $deadlineTodayTasks['title']; ?></p>
-            <p><?= $deadlineTodayTasks['description']; ?></p>
-            <p><?= $deadlineTodayTasks['deadline']; ?></p>
-            <p><?= $deadlineTodayTasks['completed']; ?></p>
-            <div class="add-task">
-                <form action="app/task/completed.php" method="post">
-                    <div class="add-task-completed">
-                        <label for="completed"></label>
-                        <input type="checkbox" name="completed" id="completed" value="1">
-                </form>
+        <h3 class="heading-deadline">Deadline today: </h3>
+        <?php foreach (getTasksDeadlineToday($_SESSION['user']['id'], $database) as $deadlineTodayTasks) : ?>
+            <div class="task-container">
+                <h4 class="task-title"><?= $deadlineTodayTasks['title']; ?></h4>
+                <p class="task-description"><?= $deadlineTodayTasks['description']; ?></p>
+                <p class="task-deadline">Deadline: <?= $deadlineTodayTasks['deadline']; ?></p>
             </div>
-
         <?php endforeach; ?>
+        <!-- button to delete list -->
+        <div class="list-form">
+            <?php foreach (getTasks($_SESSION['user']['id'], $database) as $task) : ?>
+                <form action="app/task/delete.php" method="post">
+                    <div class="add-list">
+                        <label for="delete-list"></label>
+                        <input type="hidden" name="delete-list" id="delete-list" value="<?= $task['id'] ?>">
+                        <button type="submit" class="button-main">Delete <?= $task['title'] ?></button>
+                    </div>
+                </form>
+            <?php endforeach; ?>
+        </div>
+        </div>
     <?php endif; ?>
-    <!-- add button to save update task completed -->
-</article>
+
+</main>
 
 <?php require __DIR__ . '/views/footer.php'; ?>
