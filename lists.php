3aaa6e2cd0bebc56f942915c04aea9e9cd98b2d1
h-emma <emmah92@hotmail.com>
2022-01-04 12:11:41 +0100

    Change name on class

--- lists.php
+++ lists.php
@@ -1,42 +1,43 @@
 <?php require __DIR__ . '/app/autoload.php'; ?>
 <?php require __DIR__ . '/views/header.php'; ?>
 
-<article>
-    <h1>Create your to-do lists</h1>
+<main>
+    <h1>To-do lists</h1>
 
     </div>
     <!-- add title to list -->
-    <div class="list-form">
+    <div class="add-list">
+        <h2>Create new list</h2>
+
         <form action="app/list/create.php" method="post">
-            <div class="add-list">
-                <label for="title">Lists title</label>
-                <input class="form-control" type="text" name="title" id="title" placeholder="title" required>
-            </div>
+            <label for="title">Title</label>
+            <input class="form-control" type="text" name="title" id="title" placeholder="title on the list" required>
             <button type="submit" class="button-main">Add list</button>
         </form>
+
     </div>
     <!-- loop out the lists -->
+    <h3 class="list-heding">Your lists</h3>
+    <p>Get to your list by click on it</p>
     <?php
     foreach (getLists($_SESSION['user']['id'], $database) as $list) : ?>
-        <form action="list.php" method="GET">
-            <div class="list">
-                <input type="hidden" name="list-page" id="list-page" value="<?= $list['id'] ?>">
-                <input type="hidden" name="list-name" id="list-name" value="<?= $list['title'] ?>">
-                <button type="submit" class="button-main"><?= $list['title'] ?></button>
-            </div>
-        </form>
+        <div class="list-container">
+            <form action="list.php" method="GET">
+                <div class="list">
+                    <input type="hidden" name="list-page" id="list-page" value="<?= $list['id'] ?>">
+                    <input type="hidden" name="list-name" id="list-name" value="<?= $list['title'] ?>">
+                    <button type="submit" class="button-list"><?= $list['title'] ?></button>
+                </div>
+                <!-- button to delete list -->
+            </form>
+            <form action="app/list/delete.php" method="post">
+                <div class="add-list">
+                    <input type="hidden" name="delete-list" id="delete-list" value="<?= $list['id'] ?>">
+                    <button type="submit" class="button-delete">Delete</button>
+                </div>
+            </form>
+        </div>
+    <?php endforeach; ?>
+</main>
 
-    <?php endforeach; ?>
-</article>
-<!-- button to delete list -->
-<div class="list-form">
-    <?php foreach (getLists($_SESSION['user']['id'], $database) as $list) : ?>
-        <form action="app/list/delete.php" method="post">
-            <div class="add-list">
-                <input type="hidden" name="delete-list" id="delete-list" value="<?= $list['id'] ?>">
-                <button type="submit" class="button-main">Delete <?= $list['title'] ?></button>
-            </div>
-        </form>
-    <?php endforeach; ?>
-</div>
 <?php require __DIR__ . '/views/footer.php'; ?>
