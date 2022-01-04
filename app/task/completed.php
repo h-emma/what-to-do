e0d2647175857ccb3505573af7ce286fb248cd21
h-emma <emmah92@hotmail.com>
2022-01-04 12:07:18 +0100

    Add query to get list

--- app/task/completed.php
+++ app/task/completed.php
@@ -7,6 +7,7 @@ require __DIR__ . '/../autoload.php';
 if (isset($_POST['completed'])) {
     $completed = ($_POST['completed']);
     $id = $_SESSION['user']['id'];
+    $inList = ($_POST['list']);
 
     $statement = $database->prepare('UPDATE tasks SET completed = :completed WHERE id = :id');
 
@@ -14,6 +15,12 @@ if (isset($_POST['completed'])) {
     $statement->bindParam(':completed', $completed, PDO::PARAM_STR);
 
     $statement->execute();
-    $user = $statement->fetch(PDO::FETCH_ASSOC);
+
+    $statement = $database->prepare('SELECT * FROM lists WHERE id = :list');
+    $statement->bindParam(':list', $inList, PDO::PARAM_INT);
+
+    $statement->execute();
+    $list = $statement->fetch(PDO::FETCH_ASSOC);
+
+    redirect("/list.php?list-page=$inList&list-name=$list[title]");
 };
-redirect('/list.php');
