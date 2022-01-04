ab8db865c6067db350f20c31afe00694acf9b59c
h-emma <emmah92@hotmail.com>
2022-01-04 12:12:55 +0100

    Update title on button and change article to main

--- settings-for-user.php
+++ settings-for-user.php
@@ -1,24 +1,26 @@
 <?php require __DIR__ . '/app/autoload.php'; ?>
 <?php require __DIR__ . '/views/header.php'; ?>
 
-<article>
+<main>
     <h1>Change your settings</h1>
     <!-- Update email -->
     <form action="app/users/update-settings-for-user.php" method="post">
         <div class="update-email">
+            <h2>Update you email</h2>
             <label for="email">Email</label>
             <input class="form-control" type="email" name="email" id="email" placeholder="email@mail.com" required>
         </div>
-        <button type="submit" class="button-main">Update your email</button>
+        <button type="submit" class="button-main">Update</button>
     </form>
     <!-- Update password -->
     <form action="app/users/update-settings-for-user.php" method="post">
         <div class=update-password>
+            <h2>Update you password</h2>
             <label for="password">Password</label>
             <input class="form-control" type="password" name="password" id="password" placeholder="Password" required>
         </div>
-        <button type="submit" class="button-main">Update your password</button>
+        <button type="submit" class="button-main">Update</button>
     </form>
-</article>
+</main>
 
 <?php require __DIR__ . '/views/footer.php'; ?>
