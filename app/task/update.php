dbe8a7f55eff1bacdbe87adec8a707fed1843eae
h-emma <emmah92@hotmail.com>
2022-01-05 10:15:45 +0100

    Update update.php

--- app/task/update.php
+++ app/task/update.php
@@ -5,24 +5,23 @@ declare(strict_types=1);
 require __DIR__ . '/../autoload.php';
 
 if (isset($_POST['task-title'])) {
+
+    $taskId = $_POST['task'];
     $title = trim(filter_var($_POST['task-title'], FILTER_SANITIZE_STRING));
     $description = ($_POST['task-description']);
     $deadline = ($_POST['task-deadline']);
-    $completed = ($_POST['task-completed']);
-    $id = $_SESSION['user']['id'];
-    $inList = ($_POST['list']);
-    $listName = ($_POST['list-name']);
+    //$id = $_SESSION['user']['id'];
 
-    $statement = $database->prepare('UPDATE tasks SET title = :title, description = :description, deadline = :deadline, completed = :completed  WHERE :id = :id');
+    $statement = $database->prepare('UPDATE tasks SET title = :title, description = :description, deadline = :deadline WHERE id = :task_id');
 
+    $statement->bindParam(':task_id', $taskId, PDO::PARAM_INT);
     $statement->bindParam(':title', $title, PDO::PARAM_STR);
     $statement->bindParam(':description', $description, PDO::PARAM_STR);
     $statement->bindParam(':deadline', $deadline, PDO::PARAM_STR);
-    $statement->bindParam(':completed', $completed, PDO::PARAM_STR);
-    $statement->bindParam(':id', $id, PDO::PARAM_INT);
 
     $statement->execute();
-    $user = $statement->fetch(PDO::FETCH_ASSOC);
 
-    redirect("/list.php?list-page=$inList&list-name=$listName");
+    $list = getListTaskIsIn($taskId, $database);
+
+    redirect("/list.php?list-page=$list[id]&list-name=$list[title]");
 };
