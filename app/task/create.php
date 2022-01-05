d47fa9da930b805955804ad5034c8ee2d155b7e3
h-emma <emmah92@hotmail.com>
2022-01-05 09:19:59 +0100

    Add completed to statement

--- app/task/create.php
+++ app/task/create.php
@@ -8,14 +8,16 @@ if (isset($_POST['task-title'])) {
     $title = trim(filter_var($_POST['task-title'], FILTER_SANITIZE_STRING));
     $description = ($_POST['task-description']);
     $deadline = ($_POST['task-deadline']);
+    $completed = ($_POST['task-completed']);
     $inList = ($_POST['list']);
     $id = $_SESSION['user']['id'];
 
-    $statement = $database->prepare('INSERT INTO tasks (title, description, deadline, list_id, user_id) VALUES (:title, :description, :deadline, :list_id, :user_id)');
+    $statement = $database->prepare('INSERT INTO tasks (title, description, deadline, completed, list_id, user_id) VALUES (:title, :description, :deadline, :completed, :list_id, :user_id)');
 
     $statement->bindParam(':title', $title, PDO::PARAM_STR);
     $statement->bindParam(':description', $description, PDO::PARAM_STR);
     $statement->bindParam(':deadline', $deadline, PDO::PARAM_STR);
+    $statement->bindParam(':completed', $completed, PDO::PARAM_STR);
     $statement->bindParam(':list_id', $inList, PDO::PARAM_INT);
     $statement->bindParam(':user_id', $id, PDO::PARAM_INT);
 
