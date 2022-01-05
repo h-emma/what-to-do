9aaa9dfe6d90f5344c9d76e327164f7927c0725b
h-emma <emmah92@hotmail.com>
2022-01-05 11:17:28 +0100

    Add if yes or no if task is completed

--- app/task/completed.php
+++ app/task/completed.php
@@ -7,11 +7,18 @@ require __DIR__ . '/../autoload.php';
 if (isset($_POST['completed'])) {
     $completed = ($_POST['completed']);
     $id = $_SESSION['user']['id'];
+    $taskId = ($_POST['task-id']);
     $inList = ($_POST['list']);
 
     $statement = $database->prepare('UPDATE tasks SET completed = :completed WHERE id = :id');
 
-    $statement->bindParam(':id', $id, PDO::PARAM_INT);
+    if ($completed === 'YES') {
+        $completed = 'NO';
+    } else {
+        $completed = 'YES';
+    }
+
+    $statement->bindParam(':id', $taskId, PDO::PARAM_INT);
     $statement->bindParam(':completed', $completed, PDO::PARAM_STR);
 
     $statement->execute();
