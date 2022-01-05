9091f8f78318eda2d93763740b58d5222b48b4d8
h-emma <emmah92@hotmail.com>
2022-01-05 11:18:11 +0100

    Remove delete and completed button on index page

--- index.php
+++ index.php
@@ -23,22 +23,6 @@
                 <p class="task-description"><?= $deadlineTodayTasks['description']; ?></p>
                 <p class="task-deadline">Deadline: <?= $deadlineTodayTasks['deadline']; ?></p>
                 <p class="task-completed">Completed: <?= $deadlineTodayTasks['completed']; ?></p>
-                <!-- Button to delete task -->
-                <form action="app/task/delete.php" method="post">
-                    <div class="add-action-task">
-                        <label for="delete-task"></label>
-                        <input type="hidden" name="delete-task" id="delete-task" value="<?= $task['id'] ?>">
-                        <input type="hidden" name="list" id="list" value="<?= $task['list_id'] ?>">
-                        <button type="submit" class="button-delete">Delete</button>
-                </form>
-                <!-- Button to add completed task -->
-                <form action="app/task/completed.php" method="post">
-                    <label for="completed"></label>
-                    <input type="hidden" name="completed" id="completed" value="<?= $task['id'] ?>">
-                    <input type="hidden" name="list" id="list" value="<?= $task['list_id'] ?>">
-                    <button type="submit" class="button-completed">Complete</button>
-            </div>
-            </form>
             </div>
         <?php endforeach; ?>
     <?php endif; ?>
