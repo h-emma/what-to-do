14766debadc0c1e15577a3e9f1911c4226c2f7c8
h-emma <emmah92@hotmail.com>
2022-01-09 17:14:59 +0100

    Change position on links

--- views/navigation.php
+++ views/navigation.php
@@ -2,33 +2,34 @@
     <a class="nav-loga" href="#"><?= $config['title']; ?></a>
     <ul class="navbar-nav">
         <li class="nav-item">
-            <a class="nav-link <?= $_SERVER['SCRIPT_NAME'] === 'index.php' ? 'active' : ''; ?>" href="index.php">Home</a>
+            <a class="nav-link <?= $_SERVER['SCRIPT_NAME'] === 'index.php' ? 'active' : ''; ?>" href="/index.php">Home</a>
         </li>
 
         <li class="nav-item">
             <?php if (isUserLoggedIn()) : ?>
                 <a class="nav-link" href="lists.php">Lists</a>
             <?php else : ?>
-                <a class="nav-link <?= $_SERVER['SCRIPT_NAME'] === 'login.php' ? 'active' : ''; ?>" href="login.php">Login</a>
+                <a class="nav-link <?= $_SERVER['SCRIPT_NAME'] === 'login.php' ? 'active' : ''; ?>" href="/login.php">Login</a>
             <?php endif; ?>
         </li>
 
-        <li class="nav-item">
-            <?php if (isUserLoggedIn()) : ?>
-                <a class="nav-link" href="tasks.php">Tasks</a>
-            <?php endif; ?>
 
         <li class="nav-item">
             <?php if (isUserLoggedIn()) : ?>
-                <a class="nav-link" href="settings.php">Settings</a>
+                <a class="nav-link" href="/tasks.php">Tasks</a>
             <?php else : ?>
-                <a class="nav-link <?= $_SERVER['SCRIPT_NAME'] === 'create-account.php' ? 'active' : ''; ?>" href="create-account.php">Create an account</a>
+                <a class="nav-link <?= $_SERVER['SCRIPT_NAME'] === 'create-account.php' ? 'active' : ''; ?>" href="/create-account.php">Create an account</a>
             <?php endif; ?>
         </li>
 
         <li class="nav-item">
             <?php if (isUserLoggedIn()) : ?>
-                <a class="nav-link" href="app/users/logout.php">Log out</a>
+                <a class="nav-link" href="/settings.php">Settings</a>
+            <?php endif; ?>
+
+        <li class="nav-item">
+            <?php if (isUserLoggedIn()) : ?>
+                <a class="nav-link" href="/app/users/logout.php">Log out</a>
             <?php endif; ?>
     </ul>
     <div class="hamburger">
