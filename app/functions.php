189fdf8406ce71f50bfd8473fe523c2e42047794
h-emma <emmah92@hotmail.com>
2022-01-10 14:39:22 +0100

    Add type declarations

--- app/functions.php
+++ app/functions.php
@@ -7,25 +7,25 @@ function redirect(string $path)
 {
     header("Location: ${path}");
     exit;
-};
+}
 // Functions for when user is logged in
-function isUserLoggedIn()
+function isUserLoggedIn(): bool
 {
     $loggedIn = isset($_SESSION['user']);
     return $loggedIn;
-};
+}
 // Function that get the lists
-function getLists($id, $database)
+function getLists(int $id, PDO $database): array
 {
     $statement = $database->query('SELECT * FROM lists WHERE user_id = :user_id;');
     $statement->bindParam(':user_id', $id, PDO::PARAM_INT);
     $statement->execute();
     $lists = $statement->fetchAll(PDO::FETCH_ASSOC);
     return $lists;
-};
+}
 
 // Function that get the list
-function getList($id, $listId, $database)
+function getList(int $id, int $listId, PDO $database): array
 {
     $statement = $database->query('SELECT * FROM lists WHERE user_id = :user_id AND id = :id;');
     $statement->bindParam(':user_id', $id, PDO::PARAM_INT);
@@ -33,18 +33,18 @@ function getList($id, $listId, $database)
     $statement->execute();
     $list = $statement->fetch(PDO::FETCH_ASSOC);
     return $list;
-};
+}
 // Function that get all tasks
-function getAllTasks($id, $database)
+function getAllTasks(int $id, PDO $database): array
 {
     $statement = $database->query('SELECT * FROM tasks WHERE user_id = :user_id;');
     $statement->bindParam(':user_id', $id, PDO::PARAM_INT);
     $statement->execute();
     $allTasks = $statement->fetchAll(PDO::FETCH_ASSOC);
     return $allTasks;
-};
+}
 // Funciton that get tasks in the list
-function getTasksInList($id, $database)
+function getTasksInList(int $id, PDO $database): array
 {
     $statement = $database->query('SELECT * FROM tasks WHERE user_id = :user_id AND list_id = :list_id;');
     $statement->bindParam(':list_id', $_GET['list-page'], PDO::PARAM_INT);
@@ -52,9 +52,9 @@ function getTasksInList($id, $database)
     $statement->execute();
     $listTasks = $statement->fetchAll(PDO::FETCH_ASSOC);
     return $listTasks;
-};
+}
 // Function that get list where task is in
-function getListTaskIsIn($taskId, $database)
+function getListTaskIsIn(int $taskId, PDO $database): array
 {
     $statement = $database->prepare('SELECT lists.id, lists.title FROM tasks INNER JOIN lists ON tasks.list_id = lists.id WHERE tasks.id = :task_id');
     $statement->bindParam(':task_id', $taskId, PDO::PARAM_INT);
@@ -63,7 +63,7 @@ function getListTaskIsIn($taskId, $database)
 }
 
 // Function that get all tasks that have deadline today
-function getTasksDeadlineToday($id, $database)
+function getTasksDeadlineToday(int $id, PDO $database): array
 {
     $statement = $database->query('SELECT * FROM tasks WHERE user_id = :user_id');
     $statement->bindParam(':user_id', $id, PDO::PARAM_INT);
@@ -73,4 +73,4 @@ function getTasksDeadlineToday($id, $database)
         return $deadlineTodayTask['deadline'] === date('Y-m-d');
     });
     return $deadlineTodayTasks;
-};
+}
